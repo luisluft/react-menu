844a5222dce419a5b9a4f8368e9704bef0ceb872
luisluft <luberluft@gmail.com>
2021-09-17 08:11:00 -0400

    add categories buttons dynamically

--- src/Categories.js
+++ src/Categories.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems("all")}>
-        all
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
-        breakfast
-      </button>
+      {categories.map((category, index) => {
+        return (
+          <button
+            key={index}
+            className="filter-btn"
+            onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        );
+      })}
     </div>
   );
 };
