be514ee2f607dfe4f6ec41a71adfc1fb19d6a9f7
luisluft <luberluft@gmail.com>
2021-09-17 07:45:55 -0400

    add template files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+function App() {
+  return <h1>this is the app</h1>;
+}
+
+export default App;
